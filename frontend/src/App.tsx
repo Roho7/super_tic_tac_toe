5393ab68e4570f31d293adbddad4eea92542d806
Roho7 <rohosen2@gmail.com>
2023-09-04 18:40:18 +0530

    add: favicon, cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
 import GamePage from "./pages/GamePage";
-// import { AuthProvider } from "./utils/Auth";
-// import FinalBoard from "./components/FinalBoard";
 
 function App() {
   return (
     <Routes>
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Homepage />} />
       <Route path="/play" element={<GamePage />} />
     </Routes>
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,25 +1,8 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
-interface UserData {
-  id: number;
-  username: string;
-  password: string;
-}
 
 function Homepage() {
   const navigate = useNavigate();
-  const [, setData] = useState<UserData[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get("http://localhost:8000");
-        setData(res.data);
-      } catch {}
-    };
-    fetchData();
-  });
+
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-4">
       <div className="glass p-8">
